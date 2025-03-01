ea8d634087c652fd6338653afb564ee677772bac
George Sutton <46813782+georgeosutton@users.noreply.github.com>
2025-03-01 14:33:50 +0000

    feat: tweak layout styles & ensure SVG is fixed width

--- app/page.tsx
+++ app/page.tsx
@@ -12,20 +12,22 @@ export default function Home() {
           <PricingToggle />
         </div>
       </header>
-      <main className="py-20">
-        <h1 className="pt-18 pb-4 text-center text-5xl font-bold text-violet-950 dark:text-violet-500">
-          Powerful features for{" "}
-          <span className="text-violet-600 dark:text-violet-50">
-            powerful creators
-          </span>
-        </h1>
-        <p className="pb-10 text-center text-gray-600 lg:pb-[84px] dark:text-gray-400">
-          Choose a plan that’s right for you
-        </p>
-        <div className="mx-auto grid max-w-[1440px] grid-cols-[fit-content(436px)] justify-center gap-10 px-8 lg:max-w-none lg:grid-cols-[fit-content(378px)_fit-content(416px)_fit-content(378px)] lg:gap-0">
-          {plans.map((plan) => (
-            <PaymentCard key={plan.planType as string} {...plan} />
-          ))}
+      <main className="pt-16">
+        <div className="py-10 lg:py-20">
+          <h1 className="pb-4 text-center text-5xl font-bold text-violet-950 dark:text-violet-500">
+            Powerful features for{" "}
+            <span className="text-violet-600 dark:text-violet-50">
+              powerful creators
+            </span>
+          </h1>
+          <p className="pb-10 text-center text-gray-600 lg:pb-[84px] dark:text-gray-400">
+            Choose a plan that’s right for you
+          </p>
+          <div className="mx-auto grid max-w-[1440px] grid-cols-[fit-content(436px)] justify-center gap-10 px-8 lg:max-w-none lg:grid-cols-[fit-content(378px)_fit-content(416px)_fit-content(378px)] lg:gap-0">
+            {plans.map((plan) => (
+              <PaymentCard key={plan.planType as string} {...plan} />
+            ))}
+          </div>
         </div>
       </main>
     </div>
