707a8cb781b2eb4d6e0125a08f487dd7e3e5528b
George Sutton <46813782+georgeosutton@users.noreply.github.com>
2025-03-04 21:56:24 +0000

    feat: add label for switch

--- components/pricing-toggle.tsx
+++ components/pricing-toggle.tsx
@@ -3,14 +3,15 @@
 import * as React from "react";
 import { Switch } from "./switch";
 import { usePricing } from "@/contexts/PricingContext";
+import { Label } from "./label";
 
 export function PricingToggle() {
   const { toggleYearly } = usePricing();
 
   return (
     <div className="flex items-center gap-2">
-      <Switch onClick={() => toggleYearly()} />
-      Yearly Pricing
+      <Switch onClick={() => toggleYearly()} id="yearly-pricing" />
+      <Label htmlFor="yearly-pricing">Yearly Pricing</Label>
     </div>
   );
 }
